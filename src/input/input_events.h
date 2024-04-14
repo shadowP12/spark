b31beae8d4b0059f3bebb10d60ad87a561080fca
pan <592744797@qq.com>
2024-04-14 16:54:12 +0800

    1

--- src/input/input_events.h
+++ src/input/input_events.h
@@ -16,13 +16,13 @@ public:
 
     float x = 0.0f;
     float y = 0.0f;
-    float x_delta = 0.0f;
-    float y_delta = 0.0f;
+    float offset_x = 0.0f;
+    float offset_y = 0.0f;
     uint16_t button = 0;
     uint32_t window_id = 0;
     Type type = Type::UNKNOWN;
 };
 
 namespace Input {
-    Event<MouseEvent> mouse_event;
+    static Event<MouseEvent> mouse_event;
 }
